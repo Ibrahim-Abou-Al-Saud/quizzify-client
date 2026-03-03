f83a1cbf064e01e1a00495176256fdb513acb994
Ibrahim-Abou-Al-Saud <ibrahimaboalsoud1@gmail.com>
2026-03-03 22:31:34 +0200

    feature/add-home-feature-routing-&-styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { Signup } from './features/signup/signup';
 import { Login } from './features/login/login';
 import { Dashboard } from './features/dashboard/dashboard';
+import { Layout } from './layout/layout';
+import { authGuard } from './core/guards/auth-guard';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
-  {path: 'signup', component: Signup},
   {path: 'login', component: Login},
-  {path: 'dashboard', component: Dashboard}
+  {path: 'signup', component: Signup},
+  {path: 'layout', component: Layout,
+    canActivate: [authGuard],
+    children: [
+      {path: 'home', component: Home},
+      {path: 'dashboard', component: Dashboard},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+  ]},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
